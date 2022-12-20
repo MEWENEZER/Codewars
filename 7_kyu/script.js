f724ddf9749c2b126d949ceae10537bc554bcfb9
Dmitriy <ubersweet@yandex.com>
2022-12-20 22:38:11 +0300

    Completed the task "List Filtering"
    > Practiced array.filter()
    > .filter returns an array of all matching elements

--- 7_kyu/script.js
+++ 7_kyu/script.js
@@ -90,3 +90,19 @@ function descendingOrder(n) {
 var isSquare = function (n) {
   return Number.isInteger(Math.sqrt(n));
 };
+
+// List Filtering
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+function filter_list(l) {
+  return l.filter((item) => typeof item == 'number');
+  // filter returns an array of all matching elements
+}
+
+console.log(filter_list([1, 2, 'aasf', '1', '123', 123])); // [1, 2, 123]
